59d03ec659e378337d65f5a2f9b9971e1597724d
Esmail Khalifa <120841700+hossainmdismail@users.noreply.github.com>
2025-05-15 09:30:35 +0600

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -90,9 +90,10 @@
                     <div class="footer-bottom"></div>
                 </div>
                 <div class="col-xl-4 col-lg-6 col-md-6">
-                    <p class="font-sm mb-0">&copy; 2024, <strong class="text-brand">Nest</strong> - HTML Ecommerce Template <br />All rights reserved</p>
+                    <p class="font-sm mb-0">&copy; 2025, <strong class="text-brand">Pholbari</strong><br />All rights
+                        reserved</p>
                 </div>
-                <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
+                {{-- <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                     <div class="hotline d-lg-inline-flex mr-30">
                         <img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/phone-call.svg" alt="hotline" />
                         <p>1900 - 6666<span>Working 8:00 - 22:00</span></p>
@@ -105,14 +106,24 @@
                 <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                     <div class="mobile-social-icon">
                         <h6>Follow Us</h6>
-                        <a href="#"><img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-facebook-white.svg" alt="" /></a>
-                        <a href="#"><img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-twitter-white.svg" alt="" /></a>
-                        <a href="#"><img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-instagram-white.svg" alt="" /></a>
-                        <a href="#"><img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-pinterest-white.svg" alt="" /></a>
-                        <a href="#"><img src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-youtube-white.svg" alt="" /></a>
+                        <a href="#"><img
+                                src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-facebook-white.svg"
+                                alt="" /></a>
+                        <a href="#"><img
+                                src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-twitter-white.svg"
+                                alt="" /></a>
+                        <a href="#"><img
+                                src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-instagram-white.svg"
+                                alt="" /></a>
+                        <a href="#"><img
+                                src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-pinterest-white.svg"
+                                alt="" /></a>
+                        <a href="#"><img
+                                src="{{ asset('themes/pholbari') }}/imgs/theme/icons/icon-youtube-white.svg"
+                                alt="" /></a>
                     </div>
                     <p class="font-sm">Up to 15% discount on your first subscribe</p>
-                </div>
+                </div> --}}
             </div>
         </div>
     </footer>
