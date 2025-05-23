4613be7eed48a09f5f16aed4f6bb1de396cf9dde
Esmail Khalifa <120841700+hossainmdismail@users.noreply.github.com>
2025-05-24 01:48:33 +0600

    pix

--- resources/views/themes/landing/thanks.blade.php
+++ resources/views/themes/landing/thanks.blade.php
@@ -26,7 +26,7 @@
     t.src=v;s=b.getElementsByTagName(e)[0];
     s.parentNode.insertBefore(t,s)}
 (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '2152173008558849');
+fbq('init', '1730267687568116');
 
 // Track Purchase Event
 fbq('track', 'Purchase', {
@@ -43,7 +43,7 @@ fbq('track', 'Purchase', {
 </script>
 <noscript>
     <img height="1" width="1" style="display:none"
-         src="https://www.facebook.com/tr?id=2152173008558849&ev=PageView&noscript=1" />
+         src="https://www.facebook.com/tr?id=1730267687568116&ev=PageView&noscript=1" />
 </noscript>
 </head>
 
